5328b6964298b24243e127822280331ef5dd660c
R. Quazi <112246222+qriz1452@users.noreply.github.com>
2023-08-25 18:34:25 +0530

    Create test_system_frontend.sh

---
+++ test/test_system_frontend.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Frontend system tests complete"
+
+
+# Assuming your frontend is running on http://localhost:3000
+#BASE_URL="http://localhost:3000"
+
+# Test scenario 1: User registration
+#RESPONSE=$(curl -X POST -H "Content-Type: application/json" -d '{"username":"user123", "password":"pass123"}' $BASE_URL/register)
+#if [[ "$RESPONSE" != *"Registration successful"* ]]; then
+#  echo "Frontend registration test failed"
+#  exit 1
+#fi
+
+# Test scenario 2: User login
+#RESPONSE=$(curl -X POST -H "Content-Type: application/json" -d '{"username":"user123", "password":"pass123"}' $BASE_URL/login)
+#if [[ "$RESPONSE" != *"Login successful"* ]]; then
+#  echo "Frontend login test failed"
+#  exit 1
+#fi
+
+# Add more test scenarios as needed
+
+#echo "Frontend system tests complete"
